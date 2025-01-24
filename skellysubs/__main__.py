0f423ed0720c859464a187d2a57936b169f0af24
jonmatthis <jonmatthis@gmail.com>
2025-01-24 16:48:19 -0500

    whats a LFS lol

---
+++ skellysubs/__main__.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+
+from skellysubs.add_subtitles_to_video_pipeline.add_subtitles_to_video_pipeline import get_video_and_output_paths
+from skellysubs.add_subtitles_to_video_pipeline.video_annotator.annotate_video_with_subtitles_cv2_PIL import \
+    annotate_video_with_subtitles
+from skellysubs.translate_transcript_pipeline.translate_video import translate_video
+from skellysubs.translate_transcript_pipeline.translated_transcript_model import TranslatedTranscription
+
+
+async def run_video_subtitle_pipeline(video_name: str) -> None:
+
+    (subtitled_video_path,
+     video_path,
+     translation_path) = await get_video_and_output_paths(video_name=video_name)
+
+    if Path(translation_path).exists():
+        with open(translation_path, 'r', encoding='utf-8') as f:
+            transcription_json = json.load(f)
+        translation_result = TranslatedTranscription(**transcription_json)
+    else:
+        translation_result = await translate_video(video_path=video_path)
+        # Save the translation result
+        Path(video_path.replace('.mp4', '_translation.json')).write_text(translation_result.model_dump_json(indent=4), encoding='utf-8')
+
+    # Annotate the video with the translated words
+    annotate_video_with_subtitles(video_path = video_path,
+                                  translated_transcript = translation_result,
+                                  subtitled_video_path = subtitled_video_path)
+
+if __name__ == '__main__':
+    import asyncio
+    outer_video_name = str(Path("../sample_data/sample_video_short/sample_video_short").resolve())
+    # outer_video_name = str(Path("sample_data/sample_video_long/sample_video_long").resolve())
+    asyncio.run(run_video_subtitle_pipeline(video_name=outer_video_name))
+
---
+++ skellysubs/system/files_and_folder_names.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from pathlib import Path
+from time import time
+
+
+def get_skellybot_analysis_data_folder_path() -> str:
+    path  =  Path().home() / "Sync" / "skellybot-data" / "skellybot-analysis"
+    path.mkdir(parents=True, exist_ok=True)
+    return str(path)
+
+def get_gmt_offset_string():
+    # from - https://stackoverflow.com/a/53860920/14662833
+    gmt_offset_int = int(time.localtime().tm_gmtoff / 60 / 60)
+    return f"{gmt_offset_int:+}"
+
+
+def get_iso6201_time_string(timespec: str = "milliseconds", make_filename_friendly: bool = True):
+    iso6201_timestamp = datetime.now().isoformat(timespec=timespec)
+    gmt_offset_string = f"_gmt{get_gmt_offset_string()}"
+    iso6201_timestamp_w_gmt = iso6201_timestamp + gmt_offset_string
+    if make_filename_friendly:
+        iso6201_timestamp_w_gmt = iso6201_timestamp_w_gmt.replace(":", "_")
+        iso6201_timestamp_w_gmt = iso6201_timestamp_w_gmt.replace(".", "ms")
+    return iso6201_timestamp_w_gmt
+
+
+def create_new_default_recording_name():
+    full_time = get_iso6201_time_string(timespec="seconds")
+    just_hours_minutes_seconds = full_time.split("T")[1]
+    recording_name = "recording_" + just_hours_minutes_seconds
+    return recording_name
+
+
+
+
+
+def create_log_file_name():
+    return "skellybot_analysis_" + time.strftime("%Y-%m-%d_%H_%M_%S") + ".log"
+
+def get_log_file_path():
+    log_folder_path = Path(get_skellybot_analysis_data_folder_path()) / 'logs'
+    log_folder_path.mkdir(exist_ok=True, parents=True)
+    log_file_path = log_folder_path / create_log_file_name()
+    return str(log_file_path)
