5a6bcb685a54d829158f658d8341fdd0e0663575
jonmatthis <jonmatthis@gmail.com>
2025-02-03 09:04:22 -0500

    poc hf client

---
+++ skellysubs/ai_clients/huggingface_client.py
@@ -0,0 +1,23 @@
+from huggingface_hub import InferenceClient
+
+from skellysubs.utilities.load_env_variables import HUGGINGFACE_API_KEY
+
+client = InferenceClient(
+	provider="together",
+	api_key=HUGGINGFACE_API_KEY
+)
+
+messages = [
+	{
+		"role": "user",
+		"content": "Do only humans have a corticospinal tract?"
+	}
+]
+
+completion = client.chat.completions.create(
+    model="deepseek-ai/DeepSeek-R1", 
+	messages=messages, 
+	max_tokens=500
+)
+
+print(completion.choices[0].message.content)
