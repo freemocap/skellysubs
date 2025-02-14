ee796db0f3aa4831454616e02311696626a1ac9d
jonmatthis <jonmatthis@gmail.com>
2025-02-13 20:14:37 -0500

    don't require HF key

--- skellysubs/utilities/load_env_variables.py
+++ skellysubs/utilities/load_env_variables.py
@@ -9,6 +9,6 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 if not OPENAI_API_KEY:
     raise ValueError("Please set OPENAI_API_KEY in your .env file")
 
-HUGGINGFACE_API_KEY = os.getenv('HUGGINGFACE_API_KEY')
-if not HUGGINGFACE_API_KEY:
-    raise ValueError("Please set HUGGINGFACE_API_KEY in your .env file")
+# HUGGINGFACE_API_KEY = os.getenv('HUGGINGFACE_API_KEY')
+# if not HUGGINGFACE_API_KEY:
+#     raise ValueError("Please set HUGGINGFACE_API_KEY in your .env file")
