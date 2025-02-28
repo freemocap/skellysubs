64379c6a359cfbeb6833117988c70e1428bf6863
jonmatthis <jonmatthis@gmail.com>
2025-02-28 16:57:20 -0500

    can save transcrfipt

--- skellysubs-ui/src/components/button-components/TranscribeButton.tsx
+++ skellysubs-ui/src/components/button-components/TranscribeButton.tsx
@@ -5,6 +5,8 @@ import {
 } from "../../store/slices/processingStatusSlice"
 import { Box, Button, Typography } from "@mui/material"
 import { transcribeAudioThunk } from "../../store/thunks"
+import extendedPaperbaseTheme from "../../layout/paperbase_theme/paperbase-theme"
+import type React from "react"
 
 const TranscribeButton: React.FC = () => {
   const dispatch = useAppDispatch()
@@ -16,12 +18,38 @@ const TranscribeButton: React.FC = () => {
     dispatch(transcribeAudioThunk()) // No argument needed
   }
 
+  const handleDownloadClick = () => {
+    const json = JSON.stringify(processingContext.transcription, null, 2)
+    const blob = new Blob([json], { type: "application/json" })
+    const url = URL.createObjectURL(blob)
+
+    const a = document.createElement("a")
+    a.href = url
+    a.download = "transcription.json"
+    a.click()
+
+    // Clean up the URL object
+    URL.revokeObjectURL(url)
+  }
+
   return (
-    <Box>
+    <Box
+      sx={{
+        m: 3,
+        p: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        borderStyle: "solid",
+        borderColor: "#aa00aa",
+        borderWidth: "1px",
+        borderRadius: 2,
+      }}
+    >
       <Button
         variant="contained"
         color="secondary"
-        sx={{ mt: 2 }}
+        sx={{ m: 2 }}
         onClick={handleTranscribeClick}
         disabled={!isReady}
       >
@@ -29,9 +57,21 @@ const TranscribeButton: React.FC = () => {
       </Button>
 
       {processingContext.transcription && (
-        <Typography>
-          {processingContext.transcription.text}
-        </Typography>
+        <>
+          <Typography>{processingContext.transcription.text}</Typography>
+          <Button
+            variant="contained"
+            onClick={handleDownloadClick}
+            sx={{
+              backgroundColor: extendedPaperbaseTheme.palette.primary.light,
+              borderColor: "#222222",
+              borderStyle: "solid",
+              borderWidth: "1px",
+            }}
+          >
+            Download transcription results (openai verbose json format)
+          </Button>
+        </>
       )}
     </Box>
   )
